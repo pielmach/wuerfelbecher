c8df32da19ad59dbfaf79f7ad7fe1e023bc983c2
Michael Prim <pielmach@outlook.de>
2021-06-06 18:37:06 +0200

    Refactor the actual bot initialization into wuerfelbecher and test to prevent failure as in v1.0.1

---
+++ wuerfelbecher/bot.py
@@ -0,0 +1,63 @@
+from discord.ext import commands as discord_commands
+import os
+from . import commands as wuerfelbecher_commands
+
+
+def help_message() -> str:
+    return '''
+    The Würfelbecher Bot knows about:
+    > !help - This command
+    > !roll *DicePattern* or !r *DicePattern* - Will roll dices according to given pattern. Multiple patterns split by a space are also possible.
+    > !stats *DiceType* - Will report stats on given dice type
+    > 
+    > *DiceType* can be d or w followed by number of sides. Examples: d6, w20
+    > *DicePattern* can be a number, followed by a *DiceType*, optionally followed (or preceded) by a positive or negative modificator, including just + or - sign. If any modifier is given, the dice results are summed up, including the modifier. Examples: 3w20, 1d6+4, 12+w6, 7d6+
+    '''
+
+
+def get_bot_token(secret_path: str, environment_variable: str) -> str:
+    '''
+    Strategy: First check for docker secret at secret_path, then fallback to environment_variable, then fail
+    '''
+    if os.path.exists(secret_path):
+        print("Found secret to login at {}".format(secret_path))
+        return open(secret_path).read().rstrip('\n')
+    elif environment_variable in os.environ:
+        print("Found environment variable {} to login".format(environment_variable))
+        return os.getenv(environment_variable, 'ENV_VAR_WAS_UNDEFINED')
+    else:
+        print("Failed to find access token for discord bot at {} or environment variable {}".format(
+            secret_path, environment_variable))
+        raise RuntimeError("Failed to find access token for discord bot at {} or environment variable {}".format(
+            secret_path, environment_variable))
+
+
+def setup_bot():
+    bot = discord_commands.Bot(
+        command_prefix='!',
+        description='Wuerfelbecher is a simple dice rolling bot for pen&paper roleplaying',
+        help_command=None
+    )
+
+    @bot.event
+    async def on_ready():
+        print('{0.user} online and ready to roll'.format(bot))
+
+    # TODO: consider to replace with auto-generated discord help command
+    @bot.command()
+    async def help(ctx):
+        await ctx.send(help_message())
+
+    @bot.command()
+    async def stats(ctx, *args):
+        await ctx.send(wuerfelbecher_commands.stats(' '.join(args)))
+
+    @bot.command()
+    async def roll(ctx, *args):
+        await ctx.send(ctx.author.name + ': ' + wuerfelbecher_commands.roll(' '.join(args)))
+
+    @bot.command()
+    async def r(ctx, *args):
+        await ctx.send(ctx.author.name + ': ' + wuerfelbecher_commands.roll(' '.join(args)))
+
+    return bot
--- wuerfelbecher_bot.py
+++ wuerfelbecher_bot.py
@@ -1,54 +1,6 @@
-from discord.ext import commands as discord_commands
-import os
-from wuerfelbecher import commands as wuerfelbecher_commands
-
-
-def help_message() -> str:
-    return '''
-    The Würfelbecher Bot knows about:
-    > !help - This command
-    > !roll *DicePattern* or !r *DicePattern* - Will roll dices according to given pattern. Multiple patterns split by a space are also possible.
-    > !stats *DiceType* - Will report stats on given dice type
-    > 
-    > *DiceType* can be d or w followed by number of sides. Examples: d6, w20
-    > *DicePattern* can be a number, followed by a *DiceType*, optionally followed (or preceded) by a positive or negative modificator, including just + or - sign. If any modifier is given, the dice results are summed up, including the modifier. Examples: 3w20, 1d6+4, 12+w6, 7d6+
-    '''
+from wuerfelbecher import bot as wuerfelbecher_bot
 
 
 if __name__ == '__main__':
-    bot = discord_commands.Bot(
-        command_prefix='!',
-        description='Wuerfelbecher is a simple dice rolling bot for pen&paper roleplaying',
-        help_command=None
-    )
-
-    @bot.event
-    async def on_ready():
-        print('{0.user} online and ready to roll'.format(bot))
-
-    # TODO: consider to replace with auto-generated discord help command
-    @bot.command()
-    async def help(ctx):
-        await ctx.send(help_message())
-
-    @bot.command()
-    async def stats(ctx, *args):
-        await ctx.send(wuerfelbecher_commands.stats(' '.join(args)))
-
-    @bot.command()
-    async def roll(ctx, *args):
-        await ctx.send(ctx.author.name + ': ' + wuerfelbecher_commands.roll(' '.join(args)))
-
-    @bot.command()
-    async def r(ctx, *args):
-        await ctx.send(ctx.author.name + ': ' + wuerfelbecher_commands.roll(' '.join(args)))
-
-    # Strategy: First check for docker secret, then fallback to environment variable, then fail
-    if os.path.exists('/run/secrets/discord_bot_token'):
-        print("Using docker secret to login")
-        bot.run(open('/run/secrets/discord_bot_token').read().rstrip('\n'))
-    elif 'DISCORD_BOT_TOKEN' in os.environ:
-        print("Using environment variable to login")
-        bot.run(os.getenv('DISCORD_BOT_TOKEN'))
-    else:
-        print("Failed to find access token for discord bot")
+    bot = wuerfelbecher_bot.setup_bot()
+    bot.run(wuerfelbecher_bot.get_bot_token('/run/secrets/discord_bot_token', 'DISCORD_BOT_TOKEN'))
